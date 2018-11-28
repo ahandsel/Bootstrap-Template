88acc964c65a6c180ec975015ff760aa7c23efaf
jrammmy <justinramiscal@gmail.com>
2018-11-27 23:33:09 -0800

    css and js is served up correctly

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const path = __dirname + '/';
+
+app.use("/",router);
+app.use(express.static(__dirname + '/public'));
+
+router.get("/", (req,res) => {
+  res.sendFile(path + "index.html");
+});
+
+app.listen(8000, () => {
+  console.log("Live at Port 8000");
+});
